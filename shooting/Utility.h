8ce99de5be0fd1b10f27fa543fa6e60ad63a0d77
HyoJinNam <sb04152@gmail.com>
2021-01-13 00:51:46 +0900

    delete file all

--- shooting/Utility.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define negative(x) (-x) 
-
-bool mStrncpy_s(char* start, unsigned int count, const char* str, unsigned int str_count);
-
-struct Delay {
-
-};
--- shooting/mString.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-bool mStrncpy_s(char* start, int count, const char* str, int str_count);
